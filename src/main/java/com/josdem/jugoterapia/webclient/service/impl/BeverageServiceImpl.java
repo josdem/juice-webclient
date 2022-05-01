4db2c3ac442b91e80e4651832ab7383935565208
josdem <joseluis.delacruz@gmail.com>
2022-05-01 18:32:13 -0400

    Injecting application propertiez

--- src/main/java/com/josdem/jugoterapia/webclient/service/impl/BeverageServiceImpl.java
+++ src/main/java/com/josdem/jugoterapia/webclient/service/impl/BeverageServiceImpl.java
@@ -2,16 +2,20 @@ package com.josdem.jugoterapia.webclient.service.impl;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
+import com.josdem.jugoterapia.webclient.config.ApplicationProperties;
 import com.josdem.jugoterapia.webclient.model.Beverage;
 import com.josdem.jugoterapia.webclient.service.BeverageService;
+import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
+@RequiredArgsConstructor
 public class BeverageServiceImpl implements BeverageService {
 
-    private WebClient client = WebClient.create("https://webflux.josdem.io");
+    private final ApplicationProperties applicationProperties;
+    private WebClient client = WebClient.create(applicationProperties.getUrl());
 
     public Mono<Beverage> getBeverage(Long id){
         return client.get()
