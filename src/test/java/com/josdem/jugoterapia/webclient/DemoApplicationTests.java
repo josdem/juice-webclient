591ae203f403a1477b161d17576547a64c6a0a22
josdem <joseluis.delacruz@gmail.com>
2022-05-01 17:58:45 -0400

    Starting new webclient library project :)

---
+++ src/test/java/com/josdem/jugoterapia/webclient/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josdem.jugoterapia.webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
