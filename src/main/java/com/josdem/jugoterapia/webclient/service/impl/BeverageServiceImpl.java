d075a23e093ea06d73898b79a0b57cf93cb2feea
josdem <joseluis.delacruz@gmail.com>
2022-05-02 16:33:01 -0400

    Adding webclient configuration

--- src/main/java/com/josdem/jugoterapia/webclient/service/impl/BeverageServiceImpl.java
+++ src/main/java/com/josdem/jugoterapia/webclient/service/impl/BeverageServiceImpl.java
@@ -1,31 +1,23 @@
 package com.josdem.jugoterapia.webclient.service.impl;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-
 import com.josdem.jugoterapia.webclient.config.ApplicationProperties;
 import com.josdem.jugoterapia.webclient.model.Beverage;
 import com.josdem.jugoterapia.webclient.service.BeverageService;
 import lombok.RequiredArgsConstructor;
-import reactor.core.publisher.Mono;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
-import javax.annotation.PostConstruct;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @Service
 @RequiredArgsConstructor
 public class BeverageServiceImpl implements BeverageService {
 
-    private final ApplicationProperties applicationProperties;
-    private WebClient webClient;
+    private final WebClient juiceWebClient;
 
-    @PostConstruct
-    void setup(){
-        webClient = WebClient.create(applicationProperties.getUrl());
-    }
-
-    public Mono<Beverage> getBeverage(Long id){
-        return webClient.get()
+    public Mono<Beverage> getBeverage(Long id) {
+        return juiceWebClient.get()
                 .uri("/beverages/{id}", id).accept(APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(Beverage.class);
--- src/main/java/com/josdem/jugoterapia/webclient/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/josdem/jugoterapia/webclient/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,5 @@
 package com.josdem.jugoterapia.webclient.service.impl;
 
-import com.josdem.jugoterapia.webclient.config.ApplicationProperties;
 import com.josdem.jugoterapia.webclient.model.Category;
 import com.josdem.jugoterapia.webclient.service.CategoryService;
 import lombok.RequiredArgsConstructor;
@@ -8,25 +7,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
-import javax.annotation.PostConstruct;
-
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
 
-    private final ApplicationProperties applicationProperties;
-    private WebClient webClient;
-
-    @PostConstruct
-    void setup() {
-        webClient = WebClient.create(applicationProperties.getUrl());
-    }
+    private final WebClient juiceWebClient;
 
     @Override
     public Flux<Category> getCategoriesByLanguage(String language) {
-        return webClient.get()
+        return juiceWebClient.get()
                 .uri("/categories/{language}", language).accept(APPLICATION_JSON)
                 .retrieve()
                 .bodyToFlux(Category.class);
