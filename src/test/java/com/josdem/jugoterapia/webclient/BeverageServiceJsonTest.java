a6a0fc0629cbbadfc2c6f6df0bc1ef0b9f0820fa
josdem <joseluis.delacruz@gmail.com>
2022-05-04 14:38:18 -0400

    #3 Adding missing test

---
+++ src/test/java/com/josdem/jugoterapia/webclient/BeverageServiceJsonTest.java
@@ -0,0 +1,42 @@
+package com.josdem.jugoterapia.webclient;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.josdem.jugoterapia.webclient.config.DataProperties;
+import com.josdem.jugoterapia.webclient.service.BeverageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@Slf4j
+@SpringBootTest
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class BeverageServiceJsonTest {
+  private final BeverageService beverageService;
+  private final DataProperties dataProperties;
+
+  @Test
+  @DisplayName("it gets beverage as json")
+  void shouldValidateBeverageAsJson(TestInfo testInfo) {
+    log.info("Running: {}", testInfo.getDisplayName());
+    Mono<JsonNode> publisher = beverageService.getBeverageAsJson(85L);
+    StepVerifier.create(publisher)
+        .assertNext(
+            beverage -> {
+              assertAll(
+                  "beverage",
+                  () ->
+                      assertEquals(
+                          dataProperties.getBeverage().getId(), beverage.get("id").asLong()));
+            })
+        .verifyComplete();
+  }
+}
