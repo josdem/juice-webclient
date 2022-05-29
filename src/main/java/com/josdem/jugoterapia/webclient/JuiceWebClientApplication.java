4877bd010405d727308596be0e3d3c60cde4a6b0
josdem <joseluis.delacruz@gmail.com>
2022-05-29 09:24:06 -0400

    Deleting unused code

--- src/main/java/com/josdem/jugoterapia/webclient/JuiceWebClientApplication.java
+++ src/main/java/com/josdem/jugoterapia/webclient/JuiceWebClientApplication.java
@@ -1,30 +1,12 @@
 package com.josdem.jugoterapia.webclient;
 
-import com.josdem.jugoterapia.webclient.service.BeverageService;
-import com.josdem.jugoterapia.webclient.service.CategoryService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-@Slf4j
 @SpringBootApplication
 public class JuiceWebClientApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(JuiceWebClientApplication.class, args);
-  }
-
-  @Bean
-  CommandLineRunner run(CategoryService categoryService, BeverageService beverageService) {
-    return args -> {
-      log.info("Displaying categories");
-      categoryService
-          .getCategoriesByLanguage("en")
-          .subscribe(category -> log.info(category.toString()));
-      log.info("Displaying beverage by id");
-      beverageService.getBeverage(85).subscribe(beverage -> log.info(beverage.toString()));
-    };
   }
 }
