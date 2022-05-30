a35c845ac6ffa8f0f05a4284124022f0eb82a6c6
josdem <joseluis.delacruz@gmail.com>
2022-05-30 11:09:20 -0400

    #13 Deleting unused annotations

--- src/main/java/com/josdem/jugoterapia/webclient/model/Category.java
+++ src/main/java/com/josdem/jugoterapia/webclient/model/Category.java
@@ -1,14 +1,13 @@
 package com.josdem.jugoterapia.webclient.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
-@AllArgsConstructor
+@Getter
+@Setter
 @NoArgsConstructor
 public class Category {
   private Integer id;
   private String name;
-  private String i18n;
 }
