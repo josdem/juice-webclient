fdadb15ce12a586cfd828893fa94322b7f4861d0
josdem <joseluis.delacruz@gmail.com>
2022-05-30 11:14:06 -0400

    #13 Adding only required annotations

--- src/main/java/com/josdem/jugoterapia/webclient/model/Beverage.java
+++ src/main/java/com/josdem/jugoterapia/webclient/model/Beverage.java
@@ -1,11 +1,13 @@
 package com.josdem.jugoterapia.webclient.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
-@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 @NoArgsConstructor
 public class Beverage {
 
